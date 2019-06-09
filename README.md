f3a6763822bb94a4e21abcf56cdca28d20c59f9f
Break.Yang <yiqing.yang@jingchi.ai>
2019-06-08 21:08:49 -0700

    Add installation script.

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # bdsemacs
-The emacs configuration repository for BreakDS
+
+The emacs configuration repository.
+
+Run `install.sh` to setup.
 
 ## TODO
 
-1. Create an installation script to automate the symbolic linking steps
+1.  The default `elpa/` installations will nor work for emacs24 (haven't tested it for emacs25 yet). May need specialization for them. Although a simple way to address this is to just remove `elpa/` and let emacs install all of them and sanity is back.
+
+
---
+++ install.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+function bdsemacs::install {
+    local DOTEMACS="$HOME/.emacs"
+    local DOTEMACSDOTD="$HOME/.emacs.d"
+    local BACKUP="$HOME/Downloads/emacs_config_backup/$(date +%s)"
+
+    if [[ -f "${DOTEMACS}" ]]; then
+	mkdir -p ${BACKUP}
+	mv ${DOTEMACS} ${BACKUP}
+	echo -e "[ ${GREEN}ok${NC} ] Backup ${DOTEMACS} to ${BACKUP}."
+    fi
+
+    if [[ -d "${DOTEMACSDOTD}" ]]; then
+	mkdir -p ${BACKUP}
+	mv ${DOTEMACSDOTD} ${BACKUP}
+	echo -e "[ ${GREEN}ok${NC} ] Backup ${DOTEMACSDOTD} to ${BACKUP}."
+    fi
+
+    # From https://stackoverflow.com/questions/59895/get-the-source-directory-of-a-bash-script-from-within-the-script-itself
+    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+    echo ${DIR}/dot.emacs.d
+    ln -s ${DIR}/dot.emacs.d ${DOTEMACSDOTD}
+
+    if [[ -d "${DOTEMACSDOTD}" ]]; then
+	echo -e "[${GREEN}done${NC}] Successfully installed."
+    else
+	echo -e "[${RED}FAIL${NC}] Installation abort."
+    fi
+}
+
+bdsemacs::install
